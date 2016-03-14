c63ed20bccbab389136a7b97c600c6af8f99b051
Frank Chiang <chiangf@users.noreply.github.com>
2016-03-14 19:56:40 -0400

    Bump to 0.2.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Elasticsearch',
-    version='0.2.3',
+    version='0.2.4',
     url='https://github.com/chiangf/Flask-Elasticsearch',
     license='MIT',
     author='Frank Chiang',
